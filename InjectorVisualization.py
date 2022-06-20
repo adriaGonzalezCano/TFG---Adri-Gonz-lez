1874c4410133504a42e4705c447bdaf3896b46f6
Adrià González <107857837+adriaGonzalezCano@users.noreply.github.com>
2022-06-20 17:03:58 +0200

    Add files via upload

--- InjectorVisualization.py
+++ InjectorVisualization.py
@@ -1,6 +1,5 @@
 from time import time
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from os import listdir
 import os
 import numpy as np
@@ -11,8 +10,6 @@ PATH2DATA = "./data/"
 
 SAVE_RESULTS = True
 
-VISUALIZATION_3D = False
-
 def getListExperiments():
     return listdir(PATH2DATA)
 
@@ -22,7 +19,6 @@ def main():
 
     for experiment in listExperiments:
         PATH2POSITIONS = PATH2DATA + experiment + "/coordinates/"
-        RESULTS_3D_PATH = "./results/"+ experiment +"/3d/"
         RESULTS_2D_PATH = "./results/"+ experiment +"/2d/"
 
         #Reading the coordinates files
@@ -31,10 +27,7 @@ def main():
         print(coordinates_files)
 
         #Creating the  results folder if not existing
-        if SAVE_RESULTS:
-            if not os.path.exists(RESULTS_3D_PATH) and VISUALIZATION_3D:
-                os.makedirs(RESULTS_3D_PATH)
-            
+        if SAVE_RESULTS:            
             if not os.path.exists(RESULTS_2D_PATH):
                 os.makedirs(RESULTS_2D_PATH)
 
@@ -43,22 +36,6 @@ def main():
 
         #Itereating over the N coordinate.dat files
         for i in range(25):
-            
-            #Preparing the 3D Visualization (Ho pots borrar per posar a l'informe)
-            if VISUALIZATION_3D:
-                fig_3d = plt.figure("3D")
-                ax_3d = fig_3d.add_subplot(projection='3d')
-                ax_3d.set_title("3D of particles motion t=0."+str(i+1).zfill(3))
-                ax_3d.set_xlabel("x")
-                ax_3d.set_ylabel("z")
-                ax_3d.set_zlabel("y")
-                ax_3d.set_xlim3d(0.0, 0.5)
-                ax_3d.set_zlim3d(-0.1, 0.1)
-                ax_3d.set_ylim3d(-0.004, 0.004)
-
-                #Es pot modificar l'angle de la visualitzacio 3d
-                ax_3d.view_init(elev=10., azim=-80)
-
             #Preparing the 2d visualization of the particles
             fig_2d = plt.figure("2D")
             ax_2d = fig_2d.add_subplot()
@@ -79,29 +56,15 @@ def main():
                 if idx > 12000:
                     break
                 if idx < old_particles_ids:
-                    if VISUALIZATION_3D:
-                        ax_3d.scatter(particle[0], particle[2], particle[1], c = color_list[idx], s = 10,
-                        linewidth = 0)
-
                     ax_2d.scatter(particle[0], particle[1], c = color_list[idx], s = 10,
                     linewidth = 0)
                 else:
                     #Checking the starting point of the particle and plotting them into the chart
                     if particle[1] < 0:
-                        if VISUALIZATION_3D:
-                            ax_3d.scatter(particle[0], particle[2], particle[1], c = 'blue', s = 10,
-                            linewidth = 0)
-
-
                         ax_2d.scatter(particle[0], particle[1], c = 'blue', s = 10,
                         linewidth = 0)
                         color_list.append('blue')
                     else:
-                        if VISUALIZATION_3D:
-                            ax_3d.scatter(particle[0], particle[2], particle[1], c = 'red', s = 10,
-                            linewidth = 0)
-
-
                         ax_2d.scatter(particle[0], particle[1], c = 'red', s = 10,
                         linewidth = 0)
 
@@ -117,12 +80,9 @@ def main():
             plt.draw() 
             plt.pause(0.03)
             if SAVE_RESULTS:
-                if VISUALIZATION_3D:
-                    fig_3d.savefig(RESULTS_3D_PATH+"timestamp_"+str(i)+'.png')
                 fig_2d.savefig(RESULTS_2D_PATH+"timestamp_"+str(i)+'.png')
 
             plt.clf()
-        #si ho descomentes (eliminant el #) pots moure l'angle de la visualitzacio 3D
         #plt.show()
         
 
