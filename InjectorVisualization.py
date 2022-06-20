265f3f3d7d4de5c26d1a1d02ff59c41e48c043b1
Adrià González <107857837+adriaGonzalezCano@users.noreply.github.com>
2022-06-20 17:08:09 +0200

    Add files via upload

--- InjectorVisualization.py
+++ InjectorVisualization.py
@@ -18,6 +18,9 @@ def main():
     listExperiments = getListExperiments()
 
     for experiment in listExperiments:
+        # if experiment not in ['30 deg']:#, '40 deg', '60 deg']:
+        #     continue
+
         PATH2POSITIONS = PATH2DATA + experiment + "/coordinates/"
         RESULTS_2D_PATH = "./results/"+ experiment +"/2d/"
 
@@ -27,7 +30,8 @@ def main():
         print(coordinates_files)
 
         #Creating the  results folder if not existing
-        if SAVE_RESULTS:            
+        if SAVE_RESULTS:
+            
             if not os.path.exists(RESULTS_2D_PATH):
                 os.makedirs(RESULTS_2D_PATH)
 
@@ -35,15 +39,18 @@ def main():
         color_list = []
 
         #Itereating over the N coordinate.dat files
-        for i in range(25):
+        for i in range(12):
+
             #Preparing the 2d visualization of the particles
-            fig_2d = plt.figure("2D")
+            fig_2d = plt.figure("2D", figsize=(11,7))
+            # plt.axis('equal')
+            plt.gca().set_aspect('equal', adjustable='box')
             ax_2d = fig_2d.add_subplot()
-            ax_2d.set_title("t=0."+str(i+1).zfill(3) + " | Injection_Angle=" + experiment)
+            ax_2d.set_title("t=0."+str(i+1).zfill(3) + "s | Injection_Angle=" + experiment)
             ax_2d.set_xlabel("x")
             ax_2d.set_ylabel("y")
-            ax_2d.set_xlim(0.06, 0.5)
-            ax_2d.set_ylim(-0.1, 0.1)
+            ax_2d.set_xlim(0.0, 0.65)
+            ax_2d.set_ylim(-0.17, 0.17)
 
             #Reading the coordinate.dat file which contains the position of each particle at time t
             print("Processing cartesian file: ", PATH2POSITIONS+"coordinates0."+str(i+1)+".csv")
@@ -53,19 +60,20 @@ def main():
             #from where it started to asign a specific colour along the visualization.
             print("Number of particles in the simulation: ", len(data))
             for idx, particle in enumerate(data):
-                if idx > 12000:
+                if idx > 30000:
                     break
                 if idx < old_particles_ids:
-                    ax_2d.scatter(particle[0], particle[1], c = color_list[idx], s = 10,
+                    ax_2d.scatter(particle[0], particle[1], c = color_list[idx], s = 2,
                     linewidth = 0)
                 else:
                     #Checking the starting point of the particle and plotting them into the chart
                     if particle[1] < 0:
-                        ax_2d.scatter(particle[0], particle[1], c = 'blue', s = 10,
+
+                        ax_2d.scatter(particle[0], particle[1], c = 'blue', s = 2,
                         linewidth = 0)
                         color_list.append('blue')
                     else:
-                        ax_2d.scatter(particle[0], particle[1], c = 'red', s = 10,
+                        ax_2d.scatter(particle[0], particle[1], c = 'red', s = 2,
                         linewidth = 0)
 
                         color_list.append('red')
@@ -74,8 +82,6 @@ def main():
             
             old_particles_ids = len(data)
 
-            print(data)
-            print(color_list)
             # draw the plot and saving it
             plt.draw() 
             plt.pause(0.03)
